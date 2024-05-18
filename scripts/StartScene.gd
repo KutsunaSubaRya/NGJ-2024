55b1c6a1538819fad948ddb5f2f8946668369ca3
SubaRya <index20010928@gmail.com>
2024-05-18 12:48:08 +0800

    feat: add visual novel timeline & plugins
    - add addons/character_creator_tool_importer
    - add addons/dialogic
    - add addons/fontawesome
    - add addons/shape_texture_2d

--- scripts/StartScene.gd
+++ scripts/StartScene.gd
@@ -10,7 +10,8 @@ func _process(delta):
 
 func _on_start_button_pressed():
 	print("start button clicked.")
-	get_tree().change_scene_to_file("res://scenes/LevelScene/levels/level_1-1.tscn")
+	Dialogic.start("res://timelines/timeline1.dtl")
+	#get_tree().change_scene_to_file("res://scenes/LevelScene/levels/level_1-1.tscn")
 	pass # Replace with function body.
 
 func _on_setting_button_pressed():
@@ -20,4 +21,5 @@ func _on_setting_button_pressed():
 
 func _on_leave_button_pressed():
 	print("leave button clicked.")
+	get_tree().quit()
 	pass # Replace with function body.
