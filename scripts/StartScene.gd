d7b2042597fb643eccb58516a6146bb6b6b70e8c
SubaRya <index20010928@gmail.com>
2024-05-18 11:47:27 +0800

    feat: add start scene page
    - add start game condition

---
+++ scripts/StartScene.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+
+func _on_start_button_pressed():
+	print("start button clicked.")
+	get_tree().change_scene_to_file("res://scenes/LevelScene/levels/level_1-1.tscn")
+	pass # Replace with function body.
+
+func _on_setting_button_pressed():
+	print("setting button clicked.")
+	pass # Replace with function body.
+
+
+func _on_leave_button_pressed():
+	print("leave button clicked.")
+	pass # Replace with function body.
