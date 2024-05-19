2e085f666cc24e14e9480466a40a5742fbbec138
SubaRya <index20010928@gmail.com>
2024-05-19 15:16:23 +0800

    feat: add title

---
+++ scripts/RichTextLabel.gd
@@ -0,0 +1,12 @@
+extends RichTextLabel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.text = "[wave amp3 freq=15 color=#ffffff]音 [color=#000000]色"
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
